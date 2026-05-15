fcf826341f07ebb91e0de5e6faa4debba2580f59
HK <50930310+geekr2013@users.noreply.github.com>
2026-05-15 15:41:12 +0900

    Refactor streamlit_app to use render_app function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,5 @@
-from app.poc_meeting_minutes import *  # noqa: F401,F403
+import streamlit as st
+from app.poc_meeting_minutes import render_app
+
+st.set_page_config(page_title="회의록 POC v2", layout="wide")
+render_app()
