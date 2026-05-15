386104349ff2a110648dd5d18ad21dca69487160
HK <50930310+geekr2013@users.noreply.github.com>
2026-05-15 15:53:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from app.poc_meeting_minutes import render_app
 
-st.set_page_config(page_title="회의록 POC v2", layout="wide")
+st.set_page_config(page_title="지시사항 추적관리", layout="wide")
 render_app()
